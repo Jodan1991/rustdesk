5fd022df1e421ee8b59b6771c9e57bcf49772376
shi <shi@castech.local>
2025-07-22 15:23:00 +0800

    修改api服务

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "192.168.0.47".to_owned()
+    "http://192.168.0.47:21114".to_owned()
 }
 
 #[inline]
